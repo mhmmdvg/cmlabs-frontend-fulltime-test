3c8f12068837164dcf8839522a9325d4c98a5a68
Muhammad Vikri <muhammadvikrii99@gmail.com>
2024-01-27 14:41:05 +0700

    fix: footer design

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="flex flex-row items-center justify-center bg-gray-200 w-screen py-1 border-t border-gray-300">
+    <footer className="flex flex-row items-center justify-center bg-gray-200 w-screen py-6 border-t border-gray-300">
       <p className="text-black font-sans text-sm">
         <span
-          className="hover:cursor-pointer"
+          className="hover:cursor-pointer hover:text-blue-500 hover:underline hover:underline-offset-1"
           onClick={() => window.open('https://mhmmdvg.vercel.app/', '_blank')}
         >
           Muhammad Vikri
